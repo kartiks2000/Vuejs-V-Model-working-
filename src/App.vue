8a0813aafc702a1cda9052b69f6e85a5f8de9a19
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-07-19 11:05:28 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Implementing two way data binding without using V-Model -:</h1>
+    <input type="text" :value="myinput" @input="changing"/>
+    <h2>{{myinput}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data:function(){
+      return{
+        myinput : "write here"
+      }
+  },
+  methods:{
+    changing(e){
+      console.log("hi");
+      this.myinput = e.target.value;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
